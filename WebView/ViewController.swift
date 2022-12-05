54e08b7619871ae2aba24d89eec0e3694f46fdfb
Martinus Iudex <martin.richter.1990@gmail.com>
2022-12-05 10:35:22 +0100

    Initial Commit

---
+++ WebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by Martin Richter on 05.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
